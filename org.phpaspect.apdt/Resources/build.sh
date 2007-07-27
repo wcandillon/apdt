d6b97308de805913d8e8b393a2b348c147402233
wcandillon <wcandillon@9067bdce-6c33-0410-84f1-5d7602bbf417>
2007-07-27 17:38:12 +0000

    Updating ressources

--- org.phpaspect.apdt/Resources/build.sh
+++ org.phpaspect.apdt/Resources/build.sh
@@ -1,3 +1,3 @@
 jflex-1.4.1/bin/jflex completion_scanner.flex
 java -jar java-cup-11a.jar -expect 4 -parser PHPAspectParser -symbols PHPAspectSymbols  php_parser.cup
-cp PHPAspectParser.java PHPAspectSymbols.java CompletionLexerPHPAspect.java ../src/org/phpaspect/apdt/core/model/parser/
+mv PHPAspectParser.java PHPAspectSymbols.java CompletionLexerPHPAspect.java ../src/org/phpaspect/apdt/core/model/parser/
