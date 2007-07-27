f8a9d3a5a5062373b2a02cad991cd4531a0540bb
wcandillon <wcandillon@9067bdce-6c33-0410-84f1-5d7602bbf417>
2007-07-27 18:03:25 +0000

    Initial import.

--- trunk/org.phpaspect.apdt/Resources/build.sh
+++
@@ -1,3 +0,0 @@
-jflex-1.4.1/bin/jflex completion_scanner.flex
-java -jar java-cup-11a.jar -expect 4 -parser PHPAspectParser -symbols PHPAspectSymbols  php_parser.cup
-mv PHPAspectParser.java PHPAspectSymbols.java CompletionLexerPHPAspect.java ../src/org/phpaspect/apdt/core/model/parser/
