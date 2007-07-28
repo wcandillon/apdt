782b817a452a95cfe8c988c6f3e7eaa5af7ed28f
wcandillon <wcandillon@9067bdce-6c33-0410-84f1-5d7602bbf417>
2007-07-28 10:58:36 +0000

    Initial import.

--- trunk/org.phpaspect.apdt.tests/src/org/phpaspect/apdt/tests/parser/TestPHPAspectParser.java
+++ trunk/org.phpaspect.apdt.tests/src/org/phpaspect/apdt/tests/parser/TestPHPAspectParser.java
@@ -1,6 +1,12 @@
 package org.phpaspect.apdt.tests.parser;
+
 import junit.framework.TestCase;
+import org.junit.Test;
 
 public class TestPHPAspectParser extends TestCase {
 
+	@Test
+	public void parseHelloWorld(){
+		
+	}
 }
