56efd16ee2015ebd5cb7f64969c5c2f30d3bd9bf
wcandillon <wcandillon@9067bdce-6c33-0410-84f1-5d7602bbf417>
2007-07-31 01:48:24 +0000

    Implementing the phpAspect model.

--- trunk/org.phpaspect.apdt/src/org/phpaspect/apdt/model/PHPAspectParserManager.java
+++ trunk/org.phpaspect.apdt/src/org/phpaspect/apdt/model/PHPAspectParserManager.java
@@ -5,14 +5,15 @@ import java.io.Reader;
 import org.eclipse.php.internal.core.phpModel.parser.PHPParserManager;
 import org.eclipse.php.internal.core.phpModel.parser.PhpParser;
 import org.eclipse.php.internal.core.phpModel.parser.CompletionLexer;
+import org.phpaspect.apdt.core.parser.PHPAspectCompletionLexer;
 import org.phpaspect.apdt.core.parser.PHPAspectParser;
 
 public class PHPAspectParserManager extends PHPParserManager{
 	protected CompletionLexer createCompletionLexer(Reader reader) {
-		return null;
+		return new PHPAspectCompletionLexer(reader);
 	}
 
 	protected PhpParser createPhpParser() {
-		return null;
+		return new PHPAspectParser();
 	}
 }
