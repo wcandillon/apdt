7c352be18268b46d42246b8432c834944df6e780
wcandillon <wcandillon@9067bdce-6c33-0410-84f1-5d7602bbf417>
2007-07-31 01:34:58 +0000

    Removing the build script by an ant build file

--- org.phpaspect.apdt/Resources/build.sh
+++
@@ -1,4 +0,0 @@
-jflex-1.3.5/bin/jflex completion_scanner.flex
-/opt/ibm/java2-i386-50/bin/java -classpath ../../org.eclipse.php.core/bin  org.eclipse.php.internal.core.phpModel.javacup.Main -expect 1 -parser PHPAspectParser -symbols PHPAspectSymbols -package org.eclipse.php.internal.core.phpModel < php_parser.cup
-php -r "include 'file_replace.php';file_replace('java_cup', 'org.eclipse.php.internal.core.phpModel.javacup', 'PHPAspectParser.java'); "
-mv PHPAspectParser.java PHPAspectSymbols.java PHPAspectCompletionLexer.java ../src/org/phpaspect/apdt/core/parser/
