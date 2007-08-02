88f1aa396514b6ed019dbbc9e1ceb57f30b91f57
wcandillon <wcandillon@9067bdce-6c33-0410-84f1-5d7602bbf417>
2007-08-02 16:13:40 +0000

    Adding/Removing phpAspect and php builders by toggling the nature

--- org.phpaspect.apdt/src/org/phpaspect/apdt/builder/PHPAspectNature.java
+++ org.phpaspect.apdt/src/org/phpaspect/apdt/builder/PHPAspectNature.java
@@ -5,6 +5,7 @@ import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IProjectDescription;
 import org.eclipse.core.resources.IProjectNature;
 import org.eclipse.core.runtime.CoreException;
+import org.eclipse.php.internal.core.project.options.PHPProjectOptions;
 
 public class PHPAspectNature implements IProjectNature {
 
@@ -21,20 +22,35 @@ public class PHPAspectNature implements IProjectNature {
 	 * @see org.eclipse.core.resources.IProjectNature#configure()
 	 */
 	public void configure() throws CoreException {
+		
 		IProjectDescription desc = project.getDescription();
 		ICommand[] commands = desc.getBuildSpec();
+		boolean hadAPHPBuilder = false;
+		
 		for (int i = 0; i < commands.length; ++i) {
 			if (commands[i].getBuilderName().equals(PHPAspectBuilder.BUILDER_ID)) {
 				return;
 			}
+			//We're replacing the PHP Builder by the PHPAspect one
+			if(commands[i].getBuilderName().equals(PHPProjectOptions.BUILDER_ID)){
+				hadAPHPBuilder = true;
+				commands[i] = desc.newCommand();
+				commands[i].setBuilderName(PHPAspectBuilder.BUILDER_ID);
+			}
 		}
-
-		ICommand[] newCommands = new ICommand[commands.length + 1];
-		System.arraycopy(commands, 0, newCommands, 0, commands.length);
-		ICommand command = desc.newCommand();
-		command.setBuilderName(PHPAspectBuilder.BUILDER_ID);
-		newCommands[newCommands.length - 1] = command;
-		desc.setBuildSpec(newCommands);
+		
+		desc.setBuildSpec(commands);
+		
+		if(!hadAPHPBuilder){
+			project.setDescription(desc, null);
+			ICommand[] newCommands = new ICommand[commands.length + 1];
+			System.arraycopy(commands, 0, newCommands, 0, commands.length);
+			ICommand command = desc.newCommand();
+			command.setBuilderName(PHPAspectBuilder.BUILDER_ID);
+			newCommands[newCommands.length - 1] = command;
+			desc.setBuildSpec(newCommands);	
+		}
+		
 		project.setDescription(desc, null);
 	}
 
@@ -46,16 +62,16 @@ public class PHPAspectNature implements IProjectNature {
 	public void deconfigure() throws CoreException {
 		IProjectDescription description = getProject().getDescription();
 		ICommand[] commands = description.getBuildSpec();
+
 		for (int i = 0; i < commands.length; ++i) {
+			//We're replacing the PHPAspect Builder by the PHP one
 			if (commands[i].getBuilderName().equals(PHPAspectBuilder.BUILDER_ID)) {
-				ICommand[] newCommands = new ICommand[commands.length - 1];
-				System.arraycopy(commands, 0, newCommands, 0, i);
-				System.arraycopy(commands, i + 1, newCommands, i,
-						commands.length - i - 1);
-				description.setBuildSpec(newCommands);
-				return;
+				commands[i] = description.newCommand();
+				commands[i].setBuilderName(PHPProjectOptions.BUILDER_ID);
 			}
 		}
+		description.setBuildSpec(commands);
+		project.setDescription(description, null);
 	}
 
 	/*
