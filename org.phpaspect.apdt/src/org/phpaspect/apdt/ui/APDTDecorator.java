cb37af39732851749fdaf1fd0cba4b931976b31b
wcandillon <wcandillon@9067bdce-6c33-0410-84f1-5d7602bbf417>
2007-08-05 13:11:01 +0000

    Add the PHPAspect Tools entry

--- org.phpaspect.apdt/src/org/phpaspect/apdt/ui/APDTDecorator.java
+++ org.phpaspect.apdt/src/org/phpaspect/apdt/ui/APDTDecorator.java
@@ -1,10 +1,15 @@
 package org.phpaspect.apdt.ui;
 
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.runtime.CoreException;
+import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.jface.viewers.ILabelDecorator;
 import org.eclipse.jface.viewers.LabelProvider;
+import org.eclipse.php.internal.core.project.PHPNature;
 import org.eclipse.swt.graphics.Image;
+import org.eclipse.ui.internal.OverlayIcon;
 import org.phpaspect.apdt.builder.PHPAspectNature;
 
 public class APDTDecorator extends LabelProvider implements ILabelDecorator {
@@ -16,12 +21,11 @@ public class APDTDecorator extends LabelProvider implements ILabelDecorator {
 	public Image decorateImage(Image image, Object object) {
 		//See extension org.phpaspect.apdt.APDTDecorator
 		IResource ressource = (IResource)object;
-		System.out.println(ressource.getName());
 		//If the project has the PHPAspect nature, we decorate the project folder and the aspect files
 		try {
 			if(ressource.getProject().hasNature(PHPAspectNature.NATURE_ID) &&
-					ressource.getType() != IResource.FOLDER){
-				System.out.println(ressource.getName());
+					ressource.getType() == IResource.PROJECT){
+				return null;
 			}
 		} catch (CoreException e) {
 			// TODO Do something ?
