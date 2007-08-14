cb094580377f34ece2f751ec25cc3d99e4290110
wcandillon <wcandillon@9067bdce-6c33-0410-84f1-5d7602bbf417>
2007-08-14 10:27:26 +0000

    Initial import.

---
+++ org.phpaspect.apdt.ui/src/org/phpaspect/apdt/internal/ui/treecontent/PHPAspectTreeContent.java
@@ -0,0 +1,65 @@
+package org.phpaspect.apdt.internal.ui.treecontent;
+
+import org.eclipse.jface.viewers.ILabelProviderListener;
+import org.eclipse.jface.viewers.Viewer;
+import org.eclipse.php.ui.treecontent.IPHPTreeContentProvider;
+import org.eclipse.swt.graphics.Image;
+
+public class PHPAspectTreeContent implements IPHPTreeContentProvider {
+
+	public Object[] getChildren(Object arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Object getParent(Object arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public boolean hasChildren(Object arg0) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public Object[] getElements(Object arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void dispose() {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void inputChanged(Viewer arg0, Object arg1, Object arg2) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public Image getImage(Object arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String getText(Object arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void addListener(ILabelProviderListener arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public boolean isLabelProperty(Object arg0, String arg1) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public void removeListener(ILabelProviderListener arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
