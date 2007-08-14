c62e563a5641cfd4109ff4b5f6f9a1de8ed5b98d
wcandillon <wcandillon@9067bdce-6c33-0410-84f1-5d7602bbf417>
2007-08-14 09:28:25 +0000

    Initial import of the PHPAspectEditor.

---
+++ org.phpaspect.apdt.ui/src/org/phpaspect/apdt/internal/ui/editor/PHPAspectEditor.java
@@ -0,0 +1,13 @@
+package org.phpaspect.apdt.internal.ui.editor;
+
+import org.eclipse.php.internal.ui.editor.PHPStructuredEditor;
+
+public class PHPAspectEditor extends PHPStructuredEditor {
+	
+	public static final String EDITOR_ID = "org.phpaspect.apdt.internal.ui.editor.PHPAspectEditor"; //$NON-NLS-1$
+
+	public PHPAspectEditor(){
+		super();
+	}
+	
+}
