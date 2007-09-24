22a6c86bec816e826fb42d02ba79c1a3cb5588fb
wcandillon <wcandillon@9067bdce-6c33-0410-84f1-5d7602bbf417>
2007-09-24 15:42:58 +0000

    Adding the new PHPAspect AST nodes.

--- trunk/org.phpaspect.apdt.core/src/org/phpaspect/apdt/internal/core/nodes/AspectDeclaration.java
+++ trunk/org.phpaspect.apdt.core/src/org/phpaspect/apdt/internal/core/nodes/AspectDeclaration.java
@@ -2,6 +2,7 @@ package org.phpaspect.apdt.internal.core.nodes;
 
 import java.util.List;
 
+import org.eclipse.php.internal.core.ast.nodes.ASTNode;
 import org.eclipse.php.internal.core.ast.nodes.Block;
 import org.eclipse.php.internal.core.ast.nodes.ClassDeclaration;
 import org.eclipse.php.internal.core.ast.nodes.Identifier;
@@ -12,7 +13,37 @@ public class AspectDeclaration extends ClassDeclaration {
 			Identifier className, Identifier superClass, List interfaces,
 			Block body) {
 		super(start, end, modifier, className, superClass, interfaces, body);
-		// TODO Auto-generated constructor stub
 	}
+	
+	public void toString(StringBuffer buffer, String tab) {
+		buffer.append(tab).append("<AspectDeclaration");
+		appendInterval(buffer);
+		buffer.append(" modifier='").append(getModifier(getModifier())).append("'>\n");
+		buffer.append(tab).append(TAB).append("<AspectName>\n");
+		getName().toString(buffer, TAB + TAB + tab);
+		buffer.append("\n");
+		buffer.append(tab).append(TAB).append("</AspectName>\n");
 
+		buffer.append(tab).append(TAB).append("<SuperClassName>\n");
+		if (getSuperClass() != null) {
+			getSuperClass().toString(buffer, TAB + TAB + tab);
+			buffer.append("\n");
+		}
+		buffer.append(tab).append(TAB).append("</SuperClassName>\n");
+
+		buffer.append(tab).append(TAB).append("<Interfaces>\n");
+		Identifier[] interfaces = getInterfaces();
+		for (int i = 0; interfaces != null && i < interfaces.length; i++) {
+			interfaces[i].toString(buffer, TAB + TAB + tab);
+			buffer.append("\n");
+		}
+		buffer.append(tab).append(TAB).append("</Interfaces>\n");
+		getBody().toString(buffer, TAB + tab);
+		buffer.append("\n");
+		buffer.append(tab).append("</AspectDeclaration>");
+	}
+	
+	public int getType() {
+		return PHPAspectASTNode.ASPECT_DECLARATION;
+	}
 }
