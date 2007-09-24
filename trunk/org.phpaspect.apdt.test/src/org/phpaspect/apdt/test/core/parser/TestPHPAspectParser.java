bad4d276ea1dd7820e9aa1ed71847de149192673
wcandillon <wcandillon@9067bdce-6c33-0410-84f1-5d7602bbf417>
2007-09-24 15:19:49 +0000

    Adding the first test case

--- trunk/org.phpaspect.apdt.test/src/org/phpaspect/apdt/test/core/parser/TestPHPAspectParser.java
+++ trunk/org.phpaspect.apdt.test/src/org/phpaspect/apdt/test/core/parser/TestPHPAspectParser.java
@@ -1,21 +1,47 @@
 package org.phpaspect.apdt.test.core.parser;
 
 import java.io.IOException;
+import java.io.StringReader;
 
+import junit.framework.Assert;
+
+import org.eclipse.php.test.infra.testcase.PhpTestCase;
+import org.junit.Test;
 import org.phpaspect.apdt.internal.core.parser.PHPAspectParserFactory;
 
-import junit.framework.TestCase;
-
-public class TestPHPAspectParser extends TestCase {
+public class TestPHPAspectParser extends PhpTestCase {
 	
-	public void testAspectDeclaration(){
+	@Test	
+	public void testDeclaration1(){
+		String str = "<?php aspect A{} ?>";
+		parseMustSucceed(str);
+	}
+	
+	@Test	
+	public void testWrongDeclaration1(){
+		String str = "<?php aspect A A{} ?>";
+		parseMustFailed(str);
+	}
+
+	private void parseMustSucceed(String str){
 		try {
-			PHPAspectParserFactory.create("aspects/declarations/simple.ap").parse();
-			assertTrue(true);
+			PHPAspectParserFactory.getParser(new StringReader(str)).parse();
 		} catch (IOException e) {
-			assertTrue(false);
+			Assert.assertTrue(false);
 		} catch (Exception e) {
-			assertTrue(false);
+			Assert.assertTrue(false);
 		}
+		Assert.assertTrue(true);
+	}
+	
+	private void parseMustFailed(String str){
+		try {
+			PHPAspectParserFactory.getParser(new StringReader(str)).parse();
+		} catch (IOException e) {
+			Assert.assertTrue(false);
+		} catch (Exception e) {
+			Assert.assertTrue(true);
+		}
+		Assert.assertTrue(false);
 	}
 }
