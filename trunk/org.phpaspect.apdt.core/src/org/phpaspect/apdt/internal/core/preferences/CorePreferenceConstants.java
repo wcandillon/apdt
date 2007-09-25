60d62499cbb80841092d61f91f9dbfd1b9e0ba24
wcandillon <wcandillon@9067bdce-6c33-0410-84f1-5d7602bbf417>
2007-09-25 08:56:10 +0000

    Core preferences constants.

---
+++ trunk/org.phpaspect.apdt.core/src/org/phpaspect/apdt/internal/core/preferences/CorePreferenceConstants.java
@@ -0,0 +1,9 @@
+package org.phpaspect.apdt.internal.core.preferences;
+
+import org.phpaspect.apdt.internal.core.APDTCorePlugin;
+
+public class CorePreferenceConstants {
+	public interface Keys{
+		public static final String PHPASPECT_INTERPRETER = APDTCorePlugin.PLUGIN_ID+".phpAspectInterpreter";
+	}
+}
