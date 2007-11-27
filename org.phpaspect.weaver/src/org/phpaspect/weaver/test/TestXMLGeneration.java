f3e695386aec0a58996678fb9392865362108b64
wcandillon <wcandillon@9067bdce-6c33-0410-84f1-5d7602bbf417>
2007-11-27 19:34:24 +0000

    Initial import.

---
+++ org.phpaspect.weaver/src/org/phpaspect/weaver/test/TestXMLGeneration.java
@@ -0,0 +1,32 @@
+package org.phpaspect.weaver.test;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.Reader;
+
+import org.phpaspect.weaver.parser.ASTGenerator;
+
+public class TestXMLGeneration {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		try {
+			Reader aspect = new FileReader(args[0]);
+			BufferedWriter writer = new BufferedWriter(new FileWriter(args[1]));
+			String xml = ASTGenerator.getXMLAstFromPHPAspect(aspect);
+		    writer.write(xml);
+		    writer.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
