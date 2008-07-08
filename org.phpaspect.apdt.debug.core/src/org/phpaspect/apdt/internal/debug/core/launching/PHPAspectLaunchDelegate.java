1f2eef1f9746a8e89f951fa2d508ead49bebe8a7
wcandillon <wcandillon@9067bdce-6c33-0410-84f1-5d7602bbf417>
2008-07-08 17:56:37 +0000

    Small update to build with PDT dev version.

--- org.phpaspect.apdt.debug.core/src/org/phpaspect/apdt/internal/debug/core/launching/PHPAspectLaunchDelegate.java
+++ org.phpaspect.apdt.debug.core/src/org/phpaspect/apdt/internal/debug/core/launching/PHPAspectLaunchDelegate.java
@@ -1,19 +1,17 @@
 package org.phpaspect.apdt.internal.debug.core.launching;
 
-import java.util.Map;
-
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.debug.core.ILaunch;
 import org.eclipse.debug.core.ILaunchConfiguration;
-import org.eclipse.php.internal.core.PHPCoreConstants;
+import org.eclipse.php.internal.debug.core.IPHPDebugConstants;
 import org.eclipse.php.internal.debug.core.launching.PHPExecutableLaunchDelegate;
 
 public class PHPAspectLaunchDelegate extends PHPExecutableLaunchDelegate {
 	public void launch(ILaunchConfiguration configuration, String mode, ILaunch launch, IProgressMonitor monitor) throws CoreException {
 		//launch.setAttribute(PHPCoreConstants.ATTR_FILE, arg1)
-		System.out.println(launch.getAttribute(PHPCoreConstants.ATTR_FILE));
-		String fileNameString = configuration.getAttribute(PHPCoreConstants.ATTR_FILE, (String) null);
+		System.out.println(launch.getAttribute(IPHPDebugConstants.ATTR_FILE_FULL_PATH));
+		String fileNameString = configuration.getAttribute(IPHPDebugConstants.ATTR_FILE_FULL_PATH, (String) null);
 		super.launch(configuration, mode, launch, monitor);
 	}
 }
