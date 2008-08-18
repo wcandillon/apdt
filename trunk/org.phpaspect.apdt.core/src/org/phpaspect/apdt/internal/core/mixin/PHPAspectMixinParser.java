e79271a1dc3a5ef278a540a4dbe01340d59dc8c6
wcandillon <wcandillon@9067bdce-6c33-0410-84f1-5d7602bbf417>
2008-08-18 23:05:27 +0000

    First commit.

---
+++ trunk/org.phpaspect.apdt.core/src/org/phpaspect/apdt/internal/core/mixin/PHPAspectMixinParser.java
@@ -0,0 +1,19 @@
+package org.phpaspect.apdt.internal.core.mixin;
+
+import org.eclipse.dltk.core.ISourceModule;
+import org.eclipse.dltk.core.mixin.IMixinParser;
+import org.eclipse.dltk.core.mixin.IMixinRequestor;
+
+public class PHPAspectMixinParser  implements IMixinParser {
+
+	public void parserSourceModule(boolean signature, ISourceModule module) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void setRequirestor(IMixinRequestor requestor) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
