9415393f35d23cd4a975c6c22646e72d8de84435
wcandillon <wcandillon@9067bdce-6c33-0410-84f1-5d7602bbf417>
2008-08-18 23:05:27 +0000

    First commit.

---
+++ org.phpaspect.apdt.core/src/org/phpaspect/apdt/internal/core/APDTCorePlugin.java
@@ -0,0 +1,62 @@
+package org.phpaspect.apdt.internal.core;
+
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.ui.plugin.AbstractUIPlugin;
+import org.osgi.framework.BundleContext;
+
+public class APDTCorePlugin extends AbstractUIPlugin {
+	// The plug-in ID
+	public static final String PLUGIN_ID = "org.phpaspect.apdt.core";
+
+	public static final int INTERNAL_ERROR = 10001;
+	
+	// The shared instance
+	private static APDTCorePlugin plugin;
+	
+	/**
+	 * The constructor
+	 */
+	public APDTCorePlugin() {
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext)
+	 */
+	public void start(BundleContext context) throws Exception {
+		super.start(context);
+		plugin = this;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
+	 */
+	public void stop(BundleContext context) throws Exception {
+		plugin = null;
+		super.stop(context);
+	}
+
+	/**
+	 * Returns the shared instance
+	 *
+	 * @return the shared instance
+	 */
+	public static APDTCorePlugin getDefault() {
+		return plugin;
+	}
+	
+	public static void log(IStatus status) {
+		getDefault().getLog().log(status);
+	}
+
+	public static void log(Throwable e) {
+		log(new Status(IStatus.ERROR, PLUGIN_ID, INTERNAL_ERROR, "PHPCore plugin internal error", e)); //$NON-NLS-1$
+	}
+
+	public static void logErrorMessage(String message) {
+		log(new Status(IStatus.ERROR, PLUGIN_ID, INTERNAL_ERROR, message, null));
+	}
+
+}
