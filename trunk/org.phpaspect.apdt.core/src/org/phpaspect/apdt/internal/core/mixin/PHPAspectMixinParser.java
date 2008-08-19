9d0e0922d83d884bcf2f01b167901d572927e1d1
wcandillon <wcandillon@9067bdce-6c33-0410-84f1-5d7602bbf417>
2008-08-19 20:27:35 +0000

    Add DLTK building model.

--- trunk/org.phpaspect.apdt.core/src/org/phpaspect/apdt/internal/core/mixin/PHPAspectMixinParser.java
+++ trunk/org.phpaspect.apdt.core/src/org/phpaspect/apdt/internal/core/mixin/PHPAspectMixinParser.java
@@ -4,16 +4,16 @@ import org.eclipse.dltk.core.ISourceModule;
 import org.eclipse.dltk.core.mixin.IMixinParser;
 import org.eclipse.dltk.core.mixin.IMixinRequestor;
 
-public class PHPAspectMixinParser  implements IMixinParser {
+public class PHPAspectMixinParser implements IMixinParser {
 
 	public void parserSourceModule(boolean signature, ISourceModule module) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	public void setRequirestor(IMixinRequestor requestor) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 }
