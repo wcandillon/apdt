3d44afd5919f80404846196ca53fc2bcc0b6fb29
wcandillon <wcandillon@9067bdce-6c33-0410-84f1-5d7602bbf417>
2008-08-19 20:25:08 +0000

    Fix bug in MethodInvocationPredicate

--- org.phpaspect.weaver/src/org/phpaspect/weaver/impl/PointcutCounter.java
+++ org.phpaspect.weaver/src/org/phpaspect/weaver/impl/PointcutCounter.java
@@ -2,9 +2,9 @@ package org.phpaspect.weaver.impl;
 
 public class PointcutCounter {
 
-	private static int id = -1;
+	private static int id = 0;
 	
 	public static int getId(){
-		return id++;
+		return ++id;
 	}
 }
