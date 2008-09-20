c38b170a2e39c950ce00762b673a9b1c831b2661
wcandillon <wcandillon@9067bdce-6c33-0410-84f1-5d7602bbf417>
2008-09-20 11:47:57 +0000

    Update APDT testsuite.

--- org.phpaspect.apdt.test/src/org/phpaspect/internal/test/core/compiler/ast/parser/TestCompilerAST.java
+++ org.phpaspect.apdt.test/src/org/phpaspect/internal/test/core/compiler/ast/parser/TestCompilerAST.java
@@ -1,14 +1,16 @@
 package org.phpaspect.internal.test.core.compiler.ast.parser;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.io.Reader;
-import java.io.StringReader;
+import java.util.LinkedList;
 import java.util.List;
 
-import org.eclipse.php.internal.core.compiler.ast.nodes.ASTError;
-import org.eclipse.php.test.infra.testcase.HeadlessTest;
-import org.eclipse.php.test.infra.testcase.PhpTestCase;
+import junit.framework.TestCase;
 
-import org.junit.Test;
+import org.eclipse.php.internal.core.compiler.ast.nodes.ASTError;
 
 import org.phpaspect.apdt.internal.core.compiler.ast.parser.PHPAspectParser;
 
@@ -17,30 +19,54 @@ import org.phpaspect.weaver.ast.scanner.PHPAspectLexer;
 /**
  * Testing AST creation for PHPAspect
  */
-//@HeadlessTest
-public class TestCompilerAST extends PhpTestCase {
+public class TestCompilerAST extends TestCase {
 	
-	@Test
 	public void testAspectDeclaration() throws Exception {
-		String str = "<?php aspect Foo{ public $foo = 3;" +
-				" 				public function bar(){" +
-				"					return true;" +
-				" 				} " +
-				"			}" +
-				"//foo";
-		testAspect(str);
+		Reader source = new FileReader(new File("aspects/simple.ap"));
+		List<ASTError> errors = testAspect(source);
+		assertTrue(errors.size() == 0);
 	}
 	
-	private void testAspect(String str) throws Exception{
-		Reader reader = new StringReader(str);
+	public void testAspectSyntaxError() throws Exception{
+		Reader reader = new FileReader(new File("errors/error1.ap"));
+		List<ASTError> errors = testAspect(reader);
+		printErrors("errors/error1.ap", errors);
+		List<ASTError> expectedErrors = new LinkedList<ASTError>();
+		expectedErrors.add(new ASTError(6, 11));
+		compareErrors(errors, expectedErrors);
+	}
+	
+	private void compareErrors(List<ASTError> errors, List<ASTError> expectedErrors) {
+		assertTrue(errors.size() == expectedErrors.size());
+		for(int i=0; i<errors.size(); i++)
+		{
+			assertTrue(errors.get(i).sourceStart() == expectedErrors.get(i).sourceStart());
+			assertTrue(errors.get(i).sourceEnd() == expectedErrors.get(i).sourceEnd());
+		}
+	}
+
+	private List<ASTError> testAspect(Reader reader) throws Exception{
 		PHPAspectLexer lexer = new PHPAspectLexer(reader);
 		PHPAspectParser parser = new PHPAspectParser(lexer);
 		parser.parse();
 		List<ASTError> errors = parser.getModuleDeclaration().getAllErrors();
+		return errors;
+	}
+	
+	private void printErrors(String fileName, List<ASTError> errors) throws IOException
+	{
 		for(ASTError error: errors){
 			System.err.print("Syntax error, unexpected ");
 			char[] source = new char[error.sourceEnd()-error.sourceStart()];
-			str.getChars(error.sourceStart(), error.sourceEnd(), source, 0);
+			Reader sourceReader = new FileReader(new File(fileName));
+			String str = "\n";
+			while(sourceReader.ready())
+			{
+				str += (char)sourceReader.read();
+			}
+			System.err.println("\nStart: "+error.sourceStart());
+			System.err.println("End: "+error.sourceEnd());
+			str.getChars(error.sourceStart()+1, error.sourceEnd()+1, source, 0);
 			System.err.println(source);
 		}
 	}
