cf7ad93ecd2a4f84f6620dded2054458db3b4761
wcandillon <wcandillon@9067bdce-6c33-0410-84f1-5d7602bbf417>
2008-09-20 11:48:54 +0000

    Add PHPAspect preference page.

---
+++ org.phpaspect.apdt.ui/src/org/phpaspect/apdt/internal/ui/preferences/PHPAspectPreferences.java
@@ -0,0 +1,31 @@
+package org.phpaspect.apdt.internal.ui.preferences;
+import org.eclipse.php.internal.ui.preferences.AbstractPHPPropertyPreferencePage;
+import org.eclipse.ui.IWorkbench;
+
+public class PHPAspectPreferences extends AbstractPHPPropertyPreferencePage {
+
+	protected String getPreferenceNodeQualifier() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	protected String getPreferencePageID() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	protected String getProjectSettingsKey() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	protected String getPropertyPageID() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void init(IWorkbench arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+}
