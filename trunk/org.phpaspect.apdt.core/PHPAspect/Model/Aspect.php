7672475ab09f65e266fc3de49568ea080f68e398
wcandillon <wcandillon@9067bdce-6c33-0410-84f1-5d7602bbf417>
2009-05-18 09:12:54 +0000

    Update runtime.

---
+++ trunk/org.phpaspect.apdt.core/PHPAspect/Model/Aspect.php
@@ -0,0 +1,18 @@
+<?php
+require_once 'PHPAspect/Model/IAspect.php';
+require_once 'PHPAspect/Model/ReflectionAspect.php';
+
+abstract class Aspect implements IAspect
+{
+	private $aspect = null;
+	
+	public function getAspect()
+	{
+		if($this->aspect == null)
+		{
+			$this->aspect = new ReflectionAspect(get_class($this));
+		}
+		return $this->aspect;
+	}
+}
+?>
---
+++ trunk/org.phpaspect.apdt.core/PHPAspect/Model/ReflectionAspect.php
@@ -0,0 +1,51 @@
+<?php
+require_once 'PHPAspect/Model/Annotations/Before.php';
+require_once 'PHPAspect/Model/Annotations/Around.php';
+require_once 'PHPAspect/Model/Annotations/After.php';
+
+require_once 'PHPAspect/Model/ReflectionCodeAdvice.php';
+
+class ReflectionAspect extends ReflectionAnnotatedClass implements Reflector
+{
+	public function __construct($className)
+	{
+		parent::__construct($className);
+		if(!$this->isSubclassOf(new ReflectionAnnotatedClass('Aspect')))
+		{
+			throw new ReflectionException($className . " isn't an aspect");
+		}
+	}
+	
+	private function isCodeAdvice(ReflectionAnnotatedMethod $method)
+	{
+		return $method->hasAnnotation('Before') ||
+               $method->hasAnnotation('Around') ||
+               $method->hasAnnotation('After');	
+	}
+	
+	public function getCodeAdvice($name)
+	{
+		$codeAdvice = $this->getMethod($name);
+		if($this->isCodeAdvice($codeAdvice))
+		{
+			return new ReflectionCodeAdvice($this, $codeAdvice);
+		} else {
+			return null;
+		}
+	}
+	
+	public function getCodeAdvices($filter = null)
+	{
+		$codeAdvices = array();
+		$methods = $this->getMethods($filter);
+		foreach($methods as $method)
+		{
+			if($this->isCodeAdvice($method))
+			{
+				$codeAdvices[] = new ReflectionCodeAdvice($this, $method);
+			}
+		}
+		return $codeAdvices;
+	}
+}
+?>
