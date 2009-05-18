866d0d4500602b61577956915a56d3961e181808
wcandillon <wcandillon@9067bdce-6c33-0410-84f1-5d7602bbf417>
2009-05-18 00:35:03 +0000

    Update runtime

--- org.phpaspect.apdt.core/src/org/phpaspect/internal/core/weaver/Weaver.java
+++ org.phpaspect.apdt.core/src/org/phpaspect/internal/core/weaver/Weaver.java
@@ -1,5 +1,10 @@
 package org.phpaspect.internal.core.weaver;
 
+import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URISyntaxException;
+import java.net.URL;
 import java.util.List;
 
 import org.eclipse.core.resources.IFolder;
@@ -9,11 +14,13 @@ import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.Path;
+import org.phpaspect.apdt.core.APDTCorePlugin;
 import org.phpaspect.core.weaver.Pointcut;
 
 public class Weaver {
     
     private final static IPath weavedFolder = new Path("weaved");
+    private final static IPath runtimeFolder = weavedFolder.append("PHPAspect");
     private static Weaver instance = null;
     
     private List<Pointcut> pointcuts;
@@ -29,7 +36,7 @@ public class Weaver {
         return instance;
     }
 
-	public synchronized Weaver weave(IProject project, IProgressMonitor monitor) throws CoreException
+	public synchronized Weaver weave(IProject project, IProgressMonitor monitor) throws CoreException, IOException
 	{
 	    //Create the weaved directory
 	    IFolder root = project.getFolder(weavedFolder);
@@ -38,6 +45,7 @@ public class Weaver {
             root.delete(IResource.KEEP_HISTORY, monitor);
 	    }
 	    root.create(IResource.KEEP_HISTORY, true, monitor);
+	    project.getFolder(runtimeFolder).create(IResource.KEEP_HISTORY, true, monitor);
         //Load project's aspects
 	    AspectFilesVisitor aspectVisitor = new AspectFilesVisitor();
         project.accept(aspectVisitor);
@@ -45,7 +53,27 @@ public class Weaver {
         //Copy php project
         project.accept(new CopySourceFilesVisitor(project.getName(), monitor));
         //Weave php files
-	    root.accept(new PHPFilesVisitor(monitor, pointcuts));
+	    root.accept(new PHPFilesVisitor(monitor, pointcuts));	    //Copy the PHPAspect runtime in the weaved folder
+	    String bundleLocation = APDTCorePlugin.getDefault().getBundle().getLocation();
+	    bundleLocation = bundleLocation.substring(10);
+	    URL runtime = null;
+		try {
+			runtime = new URL(bundleLocation+"PHPAspect/");
+		} catch (MalformedURLException e1) {
+			throw new IOException("Couldn't build bundle location: "+bundleLocation);
+		}
+	    File src = null;
+	    try {
+	    	src = new File(runtime.toURI());
+	    } catch (URISyntaxException e) {
+	    	src = new File(runtime.getPath());
+		}
+	    try {
+			Utils.copyFiles(src, project.getFile(runtimeFolder).getLocation().toFile());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	    root.refreshLocal(IResource.DEPTH_INFINITE, monitor);
 		return this;
 	}
 }
