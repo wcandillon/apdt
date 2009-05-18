8b7fe5b33d45906aff129ab2927c77310ce1bd52
wcandillon <wcandillon@9067bdce-6c33-0410-84f1-5d7602bbf417>
2009-05-18 09:25:51 +0000

    Add UI capabilities to the bundle. Pop up weaving error.

--- trunk/org.phpaspect.apdt.ui/src/org/phpaspect/apdt/internal/ui/actions/WeaveAction.java
+++ trunk/org.phpaspect.apdt.ui/src/org/phpaspect/apdt/internal/ui/actions/WeaveAction.java
@@ -3,11 +3,15 @@ package org.phpaspect.apdt.internal.ui.actions;
 import java.util.List;
 
 import org.eclipse.core.resources.IProject;
-import org.eclipse.core.runtime.CoreException;
 import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.swt.widgets.MessageBox;
+import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.IObjectActionDelegate;
 import org.eclipse.ui.IWorkbenchPart;
+import org.phpaspect.apdt.core.APDTCorePlugin;
+import org.phpaspect.apdt.ui.APDTUiPlugin;
 import org.phpaspect.internal.core.weaver.Weaver;
 
 public class WeaveAction implements IObjectActionDelegate {
@@ -22,8 +26,9 @@ public class WeaveAction implements IObjectActionDelegate {
         {
         	try {
 				Weaver.getInstance().weave(project, null);
-			} catch (CoreException e) {
-				// TODO Auto-generated catch block
+			} catch (Exception e) {
+				Shell shell = APDTUiPlugin.getDefault().getWorkbench().getActiveWorkbenchWindow().getShell();
+				MessageDialog.openError(shell, "Weaving error", e.getMessage());
 				e.printStackTrace();
 			}
         }
--- trunk/org.phpaspect.apdt.ui/src/org/phpaspect/apdt/ui/APDTUiPlugin.java
+++ trunk/org.phpaspect.apdt.ui/src/org/phpaspect/apdt/ui/APDTUiPlugin.java
@@ -1,23 +1,23 @@
 package org.phpaspect.apdt.ui;
 
-import org.eclipse.core.runtime.Plugin;
+import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.osgi.framework.BundleContext;
 
 /**
  * The activator class controls the plug-in life cycle
  */
-public class Activator extends Plugin {
+public class APDTUiPlugin extends AbstractUIPlugin {
 
 	// The plug-in ID
 	public static final String PLUGIN_ID = "org.phpaspect.apdt.ui";
 
 	// The shared instance
-	private static Activator plugin;
+	private static APDTUiPlugin plugin;
 	
 	/**
 	 * The constructor
 	 */
-	public Activator() {
+	public APDTUiPlugin() {
 	}
 
 	/*
@@ -43,7 +43,7 @@ public class Activator extends Plugin {
 	 *
 	 * @return the shared instance
 	 */
-	public static Activator getDefault() {
+	public static APDTUiPlugin getDefault() {
 		return plugin;
 	}
 
