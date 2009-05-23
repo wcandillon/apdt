ade160468529777559e0dc8f5d31de6155785e12
wcandillon <wcandillon@9067bdce-6c33-0410-84f1-5d7602bbf417>
2009-05-23 11:00:12 +0000

    Add new examples.

--- trunk/org.phpaspect.apdt.ui/src/org/phpaspect/apdt/internal/ui/wizards/NewAspectWizardPage.java
+++ trunk/org.phpaspect.apdt.ui/src/org/phpaspect/apdt/internal/ui/wizards/NewAspectWizardPage.java
@@ -1,19 +1,13 @@
 package org.phpaspect.apdt.internal.ui.wizards;
 
 import org.eclipse.dltk.ui.wizards.NewSourceModulePage;
-import org.phpaspect.apdt.internal.core.APDTNature;
-import org.phpaspect.apdt.ui.APDTImages;
+import org.eclipse.php.internal.core.project.PHPNature;
 
 public class NewAspectWizardPage extends NewSourceModulePage {
 	
-	public NewAspectWizardPage()
-	{
-		setImageDescriptor(APDTImages.DESC_WIZBAN_ASPECT_FILE);
-	}
-	
 	@Override
 	protected String getRequiredNature() {
-		return APDTNature.NATURE_ID;
+		return PHPNature.ID;
 	}
 
 	@Override
@@ -24,5 +18,18 @@ public class NewAspectWizardPage extends NewSourceModulePage {
 	@Override
 	protected String getPageDescription() {
 		return "This wizard creates a new Aspect for PHPAspect project";
+	}
+
+	@Override
+	protected String getFileContent() {
+		String text = getFileText();
+		String className = Character.toUpperCase(text.charAt(0))
+				+ text.substring(1);
+		return "<?php\nrequire_once 'PHPAspect/Model/Aspect.php';\n\nclass " + className + " extends Aspect\n{\n}\n?>"; //$NON-NLS-1$ //$NON-NLS-2$
+	}
+	
+	protected String getFileName() {
+		final String fileText = getFileText();
+		return fileText + ".ap"; //$NON-NLS-1$
 	}
 }
