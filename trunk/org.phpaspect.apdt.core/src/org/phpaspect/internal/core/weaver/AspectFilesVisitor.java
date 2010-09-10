f359aeb0a0d62dfa8f86a2914e058429fd9f4873
wcandillon <wcandillon@9067bdce-6c33-0410-84f1-5d7602bbf417>
2010-09-10 11:28:21 +0000

    Fix build with DLTK 2.0

--- trunk/org.phpaspect.apdt.core/src/org/phpaspect/internal/core/weaver/AspectFilesVisitor.java
+++ trunk/org.phpaspect.apdt.core/src/org/phpaspect/internal/core/weaver/AspectFilesVisitor.java
@@ -12,7 +12,17 @@ import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.SubProgressMonitor;
 import org.eclipse.core.runtime.content.IContentDescription;
 import org.eclipse.dltk.ast.declarations.ModuleDeclaration;
+import org.eclipse.dltk.ast.parser.IModuleDeclaration;
 import org.eclipse.dltk.ast.parser.ISourceParser;
+import org.eclipse.dltk.compiler.env.ModuleSource;
+import org.eclipse.dltk.compiler.problem.IProblemReporter;
+import org.eclipse.dltk.core.DLTKCore;
+import org.eclipse.dltk.core.IModelElement;
+import org.eclipse.dltk.core.ISourceModule;
+import org.eclipse.dltk.core.SourceParserUtil;
+import org.eclipse.dltk.internal.core.builder.BuildProblemReporter;
+import org.eclipse.dltk.internal.core.util.ResourceSourceModule;
+import org.eclipse.php.internal.core.compiler.ast.parser.ASTUtils;
 import org.eclipse.php.internal.core.compiler.ast.parser.PHPSourceParserFactory;
 import org.phpaspect.apdt.core.weaver.Pointcut;
 import org.phpaspect.apdt.internal.core.APDTNature;
@@ -52,7 +62,8 @@ public class AspectFilesVisitor implements IResourceVisitor {
             {
             	monitor.beginTask("Loading aspect "+file.getName(), 1);
                 char[] content = getContent(file);
-                ModuleDeclaration module = parser.parse(file.getName().toCharArray(), content, null);
+				ISourceModule moduleSource = new ResourceSourceModule(file, file.getLocationURI());
+				ModuleDeclaration module = (ModuleDeclaration) SourceParserUtil.parse(moduleSource, new BuildProblemReporter(file));
                 AspectVisitor visitor = new AspectVisitor(content);
                 try {
                     module.traverse(visitor);
