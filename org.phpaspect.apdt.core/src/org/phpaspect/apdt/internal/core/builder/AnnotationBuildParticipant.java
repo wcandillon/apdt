8fcfe3662a244a754f8ae65c0d3b1ed37bb14a0c
wcandillon <wcandillon@9067bdce-6c33-0410-84f1-5d7602bbf417>
2010-09-10 11:28:21 +0000

    Fix build with DLTK 2.0

--- org.phpaspect.apdt.core/src/org/phpaspect/apdt/internal/core/builder/AnnotationBuildParticipant.java
+++ org.phpaspect.apdt.core/src/org/phpaspect/apdt/internal/core/builder/AnnotationBuildParticipant.java
@@ -1,10 +1,16 @@
 package org.phpaspect.apdt.internal.core.builder;
 
+import org.eclipse.core.resources.IFile;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.dltk.ast.declarations.ModuleDeclaration;
+import org.eclipse.dltk.compiler.problem.IProblemReporter;
 import org.eclipse.dltk.core.IScriptProject;
+import org.eclipse.dltk.core.ISourceModule;
+import org.eclipse.dltk.core.SourceParserUtil;
 import org.eclipse.dltk.core.builder.IBuildContext;
 import org.eclipse.dltk.core.builder.IBuildParticipant;
+import org.eclipse.dltk.internal.core.builder.BuildProblemReporter;
+import org.eclipse.dltk.internal.core.util.ResourceSourceModule;
 import org.eclipse.php.internal.core.compiler.ast.parser.PHPSourceParserFactory;
 import org.phpaspect.apdt.internal.core.visitor.AspectVisitor;
 
@@ -15,7 +21,9 @@ public class AnnotationBuildParticipant implements IBuildParticipant {
 	public void build(IBuildContext context) throws CoreException {
 		char[] fileName = context.getFile().getName().toCharArray();
 		PHPSourceParserFactory parser = new PHPSourceParserFactory();
-		ModuleDeclaration module = parser.parse(fileName, context.getContents(), new NullProblemReporter());
+		IFile file = context.getFile();
+		ISourceModule moduleSource = new ResourceSourceModule(file, file.getLocationURI());
+		ModuleDeclaration module = (ModuleDeclaration) SourceParserUtil.parse(moduleSource, new BuildProblemReporter(file));
 		AspectVisitor visitor = new AspectVisitor(context);
 		try {
 			module.traverse(visitor);
